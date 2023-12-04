ec3348f6bc062c18efa59855dbfed3b5ce9bf9c4
Alvin Bauma <alvinbauma10@gmail.com>
2023-12-04 15:02:33 +0200

    fix

--- app/page.js
+++ app/page.js
@@ -1,7 +1,41 @@
-import Image from "next/image";
+"use client";
 import styles from "./page.module.css";
 
+import { useEffect, useState } from "react";
+
 export default function Home() {
+
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setError(null);
+    // Déclenchez l'impression après le rendu de la page
+    fetch("/api/print", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // Envoyez les données nécessaires pour l'impression
+      body: JSON.stringify({
+        /* vos données d'impression ici */
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          console.log(data.message);
+          // Traitez le succès ici
+        } else {
+          setError(data.message);
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        setError("Une erreur est survenue, Veuillez réessayer " + JSON.stringify(error))
+        // Traitez l'erreur ici
+      });
+  }, []);
+
   return (
     <main className={styles.main}>
       <div className={styles.center}>
@@ -11,6 +45,7 @@ export default function Home() {
           <h4>Token</h4>
           <h1>400</h1>
         </div>
+        {error && <div style={{backgroundColor: 'red', color: 'white', padding: 10, borderRadius: 10, marginTop: 10}}><b>Erreur : {error}</b></div> }
       </div>
     </main>
   );
---
+++ pages/api/print.js
@@ -0,0 +1,44 @@
+// pages/api/print.js
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    // Importations côté serveur uniquement
+    const ThermalPrinter = require("node-thermal-printer").printer;
+    const PrinterTypes = require("node-thermal-printer").types;
+
+    try {
+      // Créez une instance de l'imprimante en spécifiant le driver nécessaire
+      const printer = new ThermalPrinter({
+        type: PrinterTypes.EPSON,
+        interface: "\\.\COM1", // Assurez-vous que c'est la bonne interface
+        // driver: require("printer"), // Assurez-vous que 'printer' est installé via npm
+        // Si vous utilisez Electron ou un environnement similaire, vous pouvez avoir besoin de 'electron-printer'.
+      });
+
+      // Vérifiez si l'imprimante est connectée
+      const isPrinterConnected = await printer.isPrinterConnected();
+      if (!isPrinterConnected) {
+        // throw new Error("L'imprimante n'est pas connectée.");
+        return res.status(200).json({ success: false, message: "L'imprimante n'est pas connectée." });
+      }
+
+      // Configurez l'imprimante et imprimez
+      printer.alignCenter();
+      printer.println("Hello World!");
+      printer.cut();
+
+      // Exécutez la commande d'impression
+      const execute = await printer.execute();
+      if (execute) {
+        return res.status(200).json({ success: true, message: "Impression réussie." });
+      } else {
+        return res.status(200).json({ success: true, message: "L'exécution de l'impression a échoué." });
+        // throw new Error("L'exécution de l'impression a échoué.");
+      }
+    } catch (error) {
+      return res.status(200).json({ success: false, message: error.message });
+    }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
